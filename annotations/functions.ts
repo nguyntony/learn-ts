9c3e9002fd5a81d84de6e2d29afeaf94278849e6
Tony Nguyen <nguyen.tony1015@gmail.com>
2021-04-21 10:40:30 -0400

    fn annotatiosn

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -1,1 +1,39 @@
-function divide() {}
+const subtract = (a: number, b: number): number => {
+  return a - b;
+};
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = function (a: number, b: number): number {
+  return a * b;
+};
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+// fn that does not return anything should return void & tech a fn that returns void can return null or undefined
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+// 'this fn will never completely finish executing', very rare to use never like this, only use when we NEVER return anything
+
+const todaysWeather = {
+  date: new Date(),
+  weather: 'sunny',
+};
+
+const logWeather = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+logWeather(todaysWeather);
